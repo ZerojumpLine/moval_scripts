ad1f366db8129beb4a66ee9908d58773f39d7b6c
Zeju Li <zejuli@Zejus-MacBook-Pro.local>
2024-05-19 16:22:31 +0100

    cifar imbalance testing

--- run_cifar10.py
+++ run_cifar10.py
@@ -34,56 +34,32 @@ numcls = 10
 
 # estimate 36 conditions
 with open('./cifar10_estim.txt', 'w') as fpr:
-    metric = metrics[0]
-    for training_cond in training_conds:
-        ext = training_cond.split('/')[-1]
-        if ext == 'baseline':
-            dataset = 'CIFAR10'
-        else:
-            dataset = f"CIFAR10{ext}"
-        valpath = f"{training_cond}/predictions_val.csv"
-        fpr.write(cmd_estim.format(dataset=dataset, numcls=numcls, metric=metric, valpath=f'"{valpath}"'))
-    
-    training_cond = training_conds[0]
-    for metric in metrics[1:]:
-        ext = training_cond.split('/')[-1]
-        if ext == 'baseline':
-            dataset = 'CIFAR10'
-        else:
-            dataset = f"CIFAR10{ext}"
-        valpath = f"{training_cond}/predictions_val.csv"
-        fpr.write(cmd_estim.format(dataset=dataset, numcls=numcls, metric=metric, valpath=f'"{valpath}"'))
+    for metric in metrics:
+        for training_cond in training_conds:
+            ext = training_cond.split('/')[-1]
+            if ext == 'baseline':
+                dataset = 'CIFAR10'
+            else:
+                dataset = f"CIFAR10{ext}"
+            valpath = f"{training_cond}/predictions_val.csv"
+            fpr.write(cmd_estim.format(dataset=dataset, numcls=numcls, metric=metric, valpath=f'"{valpath}"'))
 
 print(f'fsl_sub -q long -R 128 -l logs -t ./cifar10_estim.txt')
 
 # evaluate 36 condtions for all test conditions
 with open('./cifar10_eval.txt', 'w+') as fpr:
-    metric = metrics[0]
-    for training_cond in training_conds:
-        ext = training_cond.split('/')[-1]
-        if ext == 'baseline':
-            dataset = 'CIFAR10'
-        else:
-            dataset = f"CIFAR10{ext}"
+    for metric in metrics:
+        for training_cond in training_conds:
+            ext = training_cond.split('/')[-1]
+            if ext == 'baseline':
+                dataset = 'CIFAR10'
+            else:
+                dataset = f"CIFAR10{ext}"
 
-        for test_syn_cond in test_syn_conds:
+            for test_syn_cond in test_syn_conds:
 
-            testpath = f"{training_cond}/predictions_val_{test_syn_cond}.csv"
-            savingpath = f"./results_{dataset}_{metric}_{test_syn_cond}.txt"
-            fpr.write(cmd_eval.format(dataset=dataset, testpath=f'"{testpath}"', metric=metric, savingpath=f'"{savingpath}"'))
-
-    training_cond = training_conds[0]
-    for metric in metrics[1:]:
-        ext = training_cond.split('/')[-1]
-        if ext == 'baseline':
-            dataset = 'CIFAR10'
-        else:
-            dataset = f"CIFAR10{ext}"
-
-        for test_syn_cond in test_syn_conds:
-
-            testpath = f"{training_cond}/predictions_val_{test_syn_cond}.csv"
-            savingpath = f"./results_{dataset}_{metric}_{test_syn_cond}.txt"
-            fpr.write(cmd_eval.format(dataset=dataset, testpath=f'"{testpath}"', metric=metric, savingpath=f'"{savingpath}"'))
+                testpath = f"{training_cond}/predictions_val_{test_syn_cond}.csv"
+                savingpath = f"./results_{dataset}_{metric}_{test_syn_cond}.txt"
+                fpr.write(cmd_eval.format(dataset=dataset, testpath=f'"{testpath}"', metric=metric, savingpath=f'"{savingpath}"'))
 
 print(f'fsl_sub -q long -R 128 -l logs -t ./cifar10_eval.txt')
